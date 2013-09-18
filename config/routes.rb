a2e9177d667797b35d0ba8197573c14fe485484b
Keanu Lee <keanulee517@gmail.com>
2013-09-18 17:51:09 -0400

    redirect root to /stops

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Bus::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => redirect('/stops')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
