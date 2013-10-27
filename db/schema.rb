0791c4130ddfd521dd6a19005dc5dab2105fe874
Keanu Lee <keanulee517@gmail.com>
2013-10-27 18:32:45 -0400

    swtich to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130914211912) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "calendars", force: true do |t|
     t.string   "service_id"
     t.integer  "monday"
